179bb64bd66b5c86b52f75156652d2f902b8bdda
16646684 <arkanat.c@ku.th>
2025-02-17 16:33:12 +0700

    update js

--- index.js
+++ index.js
@@ -1,17 +1,104 @@
-// import http module เพื่อสร้าง server
-const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql2/promise');
+const app = express();
 
-const host = 'localhost' // กำหนด host ที่ server 0ะรอรับ request www.example.com
-const port = 8000 // กำหนด port ที่ server รอรับ request ที่เป็น http request
+app.use(bodyParser.json());
+const port = 8000;
 
-// กำหนดค่า้ริ่มต้นของ server
-const requestListener = function (req, res) {
-    res.writeHead(200); //ส่ง status code 200 กลับไปให้ client
-    res.end('My first server!');// ส่ง response กลับไปให้ client 
-}
+// เก็บ user
+let users = []
+let counter = 1
 
-const server = http.createServer(requestListener);//สร้าง server ด้วย http.createServer โดยส่ง requestListener ไปเป็น argument
-server.listen(port, host, () => { // กำหนดให้ server ทำงานที่ port 8000 และ host คือ localhost
-    console.log(`Server is running on http://${host}:${port}`);// แสดงข้อความว่า server กำลังทำงานอยู่ที่ http://localhost:8000
+/* GET /users สำหรับ get users ทั้งหมด
+POST /user สำหรับสร้าง user ใหม่บันทึกเข้าไป
+PUT /user/:id สำหรับแก้ไข user รายคนที่ต้องการบันทึก
+GET /user/:id สำหรับดึงข้อมูล user รายคน
+PUT /user/:id สำหรับแก้ไข user รายคนที่ต้องการบันทึก
+DELETE /user/:id สำหรับลบ user รายคนที่ต้องการลบ
+*/
+// path = GET /users5
+app.get('/testdb', (req, res) => {
+  mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'webdb',
+    port: 8830
+
+}).then((conn) => {
+  conn
+  .query('SELECT * FROM users')
+  .then((results) => {
+    res.json(results[0])
+  })
+  .catch((error) => {
+    console.log('Erro fetching users:', error.message)
+    res.status(500).json({error:'Error fetching users'})
+    })
+  })
+});
+
+app.get('/testdb-new', async (req, res) => {
+  const conn = await mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'webdb',
+    port: 8830
+  })
+  const result = await conn.query('SELECT * FROM users')
+  res.json(result[0])
 })
 
+
+// path = POST /user
+app.post('/user', (req, res) => {
+  let user = req.body;
+  user.id = counter
+  counter++;
+  users.push(user);
+  res.json({
+    message : "User created",
+    user : user
+  });
+});
+
+//path = PUT /user/:id
+app.put('/user/:id', (req, res) => {
+  let id = req.params.id;
+  let updateUser = req.body
+  //หา user จาก id ที่ส่งมา
+  let selectedIndex = users.findIndex(user => user.id == id);
+  //update user นั้น
+  if (updateUser.firstname) {
+    users[selectedIndex].firstname = updateUser.firstname || users[selectedIndex].firstname
+  }
+  if (updateUser.lastname) {
+  users[selectedIndex].lastname = updateUser.lastname || users[selectedIndex].lastname
+  }
+  res.json({
+    message : "User updated",
+    data: {
+      user: updateUser,
+      indexUpdate: selectedIndex
+    }
+  });
+}
+);
+//Path = DELETE /user/:id
+app.delete('/user/:id', (req, res) => {
+  let id = req.params.id
+  //หา index ของ user ที่ต้องการลบ
+  let selectedIndex = users.findIndex(user => user.id == id);
+  
+  users.splice(selectedIndex, 1);
+  res.json({
+    message : "Delete Completed",
+    indexDelete : selectedIndex
+  });
+});
+
+app.listen(port, (req, res) => {
+    console.log('Server is running on port' + port);
+});
